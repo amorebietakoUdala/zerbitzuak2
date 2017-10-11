fab34004f0106793506379b0f7a4eab78ff23c46
Amorebietako Udala <informatika@amorebieta.eus>
2017-10-11 10:49:49 +0200

    Estatistika atalean, javascripta hobetzea hobre mantentzeko.

--- web/js/stats.js
+++ web/js/stats.js
@@ -1,16 +1,22 @@
-$(function() {
-
-    $(document).ready(function(){
-	$('#bilatzailea').show();
-	var erakutsi = true;
-	$('#bilaketa_titulua').click(function () {
-	    if (!erakutsi) {
-		$('#bilatzailea').show();
-		erakutsi = true;
-	    } else  {
-		$('#bilatzailea').hide();
-		erakutsi = false;
-	    }
-	});    
+function View() {
+  var dom = {
+    bilatzailea: $('#bilatzailea'),
+    bilaketaTitulua: $('#bilaketa_titulua'),
+  };
+  var erakutsi = true;
+  function onTituluaClick() { 
+    dom.bilaketaTitulua.on('click', function () {
+	if (!erakutsi) {
+	    dom.bilatzailea.show();
+	    erakutsi = true;
+	} else  {
+	    dom.bilatzailea.hide();
+	    erakutsi = false;
+	}
+	return false;
     });
-});
+  }
+  return {
+    onTituluaClick: onTituluaClick
+  };
+}
